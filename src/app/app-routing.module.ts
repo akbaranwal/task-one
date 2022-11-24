99c0cfedb09abbbe689d4f5402b88ad4748bc586
Ankit Baranwal <m1064446@mindtree.com>
2022-11-24 17:56:30 +0530

    task one done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskOneComponent } from './component/task-one/task-one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/task-1', pathMatch: 'full' },
+  { path: 'task-1', component: TaskOneComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskOneComponent } from './component/task-one/task-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskOneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
